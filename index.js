f9dc65f006c58133da6771e036974f7f4330fdba
cadorn <christoph@christophdorn.com>
2013-02-15 15:39:42 -0800

    various tweaks

--- index.js
+++ index.js
@@ -10,9 +10,24 @@ const JSONLINT = require("jsonlint");
 
 var instances = {};
 
-exports.for = function(module, ns) {
-	return new PINF(module, ns);
+exports.forProgram = function(options) {
+	if (typeof options === "string") {
+		options = {
+			CWD: options
+		};
+	}
+	return function(module, ns) {
+		return new PINF(options, module, ns);
+	};
 }
+
+exports.for = exports.forProgram({
+	CWD: process.cwd(),
+	PINF_PROGRAM: process.env.PINF_PROGRAM,
+	PINF_PACKAGE: process.env.PINF_PACKAGE,
+	PINF_MODE: process.env.PINF_MODE
+});
+
 
 exports.uriToPath = function(uri) {
 	return uri.replace(/[:@#]/g, "/").replace(/[\?&=]/g, "+").replace(/\/+/g, "/").replace(/\/$/, "+");
@@ -22,18 +37,23 @@ exports.uriToFilename = function(uri) {
 	return exports.uriToPath(uri).replace(/\//g, "+");
 }
 
-var PINF = function(module, ns) {
+
+var PINF = function(options, module, ns) {
 	var self = this;
 
+	self.ENV = {};
+	for (var name in process.env) {
+		self.ENV[name] = process.env[name];
+	}
 	// These environment variables declare what to boot and in which state:
 	//   * A local filesystem path to a `program.json` file (how to boot).
-	var PINF_PROGRAM = process.env.PINF_PROGRAM || PATH.join(process.cwd(), "program.json");
+	self.ENV.PINF_PROGRAM = options.PINF_PROGRAM || PATH.join(options.CWD, "program.json");
 	//   * A local filesystem path to a `package.json` file (what to boot).
-	var PINF_PACKAGE = process.env.PINF_PACKAGE || PATH.join(process.cwd(), "package.json");
+	self.ENV.PINF_PACKAGE = options.PINF_PACKAGE || PATH.join(options.CWD, "package.json");
 	//   * A local filesystem path to a `program.rt.json` file (the state to boot in).
-	var PINF_RUNTIME = PATH.join(PINF_PROGRAM, "../.rt/program.rt.json");
+	self.ENV.PINF_RUNTIME = PATH.join(self.ENV.PINF_PROGRAM, "../.rt/program.rt.json");
 	//   * The mode the runtime should run it. Will load `program.$PINF_MODE.json`.
-	var PINF_MODE = process.env.PINF_MODE || "production";
+	self.ENV.PINF_MODE = options.PINF_MODE || "production";
 
 	if (typeof module === "string") {
 		self.module = {
@@ -53,8 +73,8 @@ var PINF = function(module, ns) {
 			env: null
 		},
 		paths: {
-			runtime: PINF_RUNTIME,
-			program: PINF_PROGRAM,
+			runtime: self.ENV.PINF_RUNTIME,
+			program: self.ENV.PINF_PROGRAM,
 			package: null,
 			data: null,
 			conf: null,
@@ -106,8 +126,8 @@ var PINF = function(module, ns) {
 			var m = json.match(/\$([A-Z0-9_]*)/g);
 			if (m) {
 				m.forEach(function(name) {
-					ASSERT(typeof process.env[name.substring(1)] === "string", "The '" + name.substring(1) + "' environment variable must be set!")
-					json = json.replace(new RegExp("\\" + name, "g"), process.env[name.substring(1)]);
+					ASSERT(typeof ENV[name.substring(1)] === "string", "The '" + name.substring(1) + "' environment variable must be set!")
+					json = json.replace(new RegExp("\\" + name, "g"), ENV[name.substring(1)]);
 				});
 			}
 			var obj = JSON.parse(json);
@@ -153,7 +173,7 @@ var PINF = function(module, ns) {
 		packageUid = formatUid(descriptor.uid);
 	});
 	//   5) /program.json
-	loadJSON(PINF_PROGRAM, function(obj) {
+	loadJSON(self.ENV.PINF_PROGRAM, function(obj) {
 		descriptor = DEEPMERGE(descriptor, obj);
 	});
 	//   4) ./.package.json
@@ -161,24 +181,25 @@ var PINF = function(module, ns) {
 		insertNamespace(obj, "config", ".");
 		insertNamespace(obj, "env", ".");
 		descriptor = DEEPMERGE(descriptor, obj);
+		packageUid = formatUid(descriptor.uid) || packageUid;
 	});
 	//   3) /.program.json
-	loadJSON(PINF_PROGRAM.replace(/\/([^\/]*)$/, "\/.$1"), function(obj) {
+	loadJSON(self.ENV.PINF_PROGRAM.replace(/\/([^\/]*)$/, "\/.$1"), function(obj) {
 		descriptor = DEEPMERGE(descriptor, obj);
 	});
 	//   2) /.rt/program.rt.json
 	//		The `rt` descriptor holds the runtime information for this instance of the program. There can always
 	//		only be one runtime instance of a program installation. If you want to boot a second, create an
 	//		inheriting program descriptor in a new directory and boot it there.
-	loadJSON(PINF_RUNTIME, function(obj) {
+	loadJSON(self.ENV.PINF_RUNTIME, function(obj) {
 		descriptor = DEEPMERGE(descriptor, obj);
 	});
 	//   1) /program.$PINF_MODE.json
-	loadJSON(PINF_PROGRAM.replace(".json", "." + PINF_MODE + ".json"), function(obj) {
+	loadJSON(self.ENV.PINF_PROGRAM.replace(".json", "." + self.ENV.PINF_MODE + ".json"), function(obj) {
 		descriptor = DEEPMERGE(descriptor, obj);
 	});
 
-	module.pinf.uid = formatUid(module.pinf.uid || descriptor.uid || PATH.dirname(packageDescriptorPath));
+	module.pinf.uid = formatUid(module.pinf.uid || packageUid || PATH.dirname(packageDescriptorPath));
 	module.pinf.ns.filename = module.pinf.ns.filename || exports.uriToFilename(module.pinf.uid + "+" + module.pinf.iid);
 	module.pinf.ns.config = module.pinf.ns.config || packageUid || ".";
 	module.pinf.ns.env = module.pinf.ns.env || module.pinf.ns.config;
@@ -189,7 +210,7 @@ var PINF = function(module, ns) {
 		if (descriptor.directories && descriptor.directories[type]) {
 			module.pinf.paths[type] = PATH.resolve(packageDescriptorPath, descriptor.directories[type]);
 		} else {
-			module.pinf.paths[type] = PATH.join(PINF_RUNTIME, "..", type, module.pinf.ns.filename);
+			module.pinf.paths[type] = PATH.join(self.ENV.PINF_RUNTIME, "..", type, module.pinf.ns.filename);
 		}
 	});
 
@@ -242,12 +263,18 @@ var PINF = function(module, ns) {
 			mergePropertyFor("env", module.pinf.ns.env);
 		}
 	}
+
+	self._credentials = descriptor.credentials || {};
 }
 
 PINF.prototype.config = function(extra) {
 	var config = DEEPMERGE(this.module.pinf.config || {}, extra || {});
 	config.pinf = this.module.pinf;
 	return config;
+}
+
+PINF.prototype.credentials = function() {
+	return this._credentials;
 }
 
 PINF.prototype.path = function(options, type, subpath, filename) {
@@ -268,15 +295,16 @@ PINF.prototype.path = function(options, type, subpath, filename) {
 // NOTE: This function may take a while to return depending on the number of runtimes
 //		 to sync across, their distance, and db used to sync.
 PINF.prototype.singleton = function(config, constructor, callback) {
+	var self = this;
 	var id = [
 	    config.pinf.uid,
 	    config.pinf.iid
 	].join(":");
 	// TODO: Keep track of instance across processes. i.e. only one process per `uid : iid`
 	function finalize(callback) {
-		return callback(null, instances[id]);
+		return callback(null, instances[self.ENV.PINF_RUNTIME][id]);
 	}
-	if (!instances[id]) {
+	if (!instances[self.ENV.PINF_RUNTIME] || !instances[self.ENV.PINF_RUNTIME][id]) {
 		try {
 			var Instance = function() {
 				this.config = config;
@@ -288,8 +316,9 @@ PINF.prototype.singleton = function(config, constructor, callback) {
 	// TODO: Attach instance pointer to persistant storage (local fs, central db, etc...)
 				return callback(null);
 			}
-			instances[id] = new Instance();
-			instances[id].__construct(function(err) {
+			if (!instances[self.ENV.PINF_RUNTIME]) instances[self.ENV.PINF_RUNTIME] = {};
+			instances[self.ENV.PINF_RUNTIME][id] = new Instance();
+			instances[self.ENV.PINF_RUNTIME][id].__construct(function(err) {
 				if (err) return callback(err);
 
 //console.log("got init");
